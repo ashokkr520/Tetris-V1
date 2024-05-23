6c550bf1bb20dc38fd73378751d5eac96ce4e950
ashokkr520 <66587938+ashokkr520@users.noreply.github.com>
2024-05-23 19:58:27 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,9 +59,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = ""
+  ami                    = "ami-02bf8ce06a8ed6092"
   instance_type          = "t2.large"
-  key_name               = "purplehaze"
+  key_name               = "kkk"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
